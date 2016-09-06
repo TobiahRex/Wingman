53cc03c063a38fc5b1637cd76dba7ba60f41fc17
TobiahRex <tobiah.rex@gmail.com>
2016-09-05 22:14:19 -0700

    updated Action/Creators.js with all register functions

--- App/Actions/Creators.js
+++ App/Actions/Creators.js
@@ -1,10 +1,22 @@
 import Types from './Types'
 
-const receivedUser = (user, seettings, location) =>
+const receivedUser = (user, settings, location) =>
 ({ type: Types.USER_RECEIVED, user, settings, location })
 
 const receivedActiveUsers = (users) =>
 ({ type: Types.ACTIVE_USERS_RECEIVED, users })
+
+const authChange = (username, email, uid, photo) =>
+({ type: Types.AUTH_CHANGE, username, email, uid, photo })
+
+const registerAttempt = () =>
+({ type: Types.REGISTER_ATTEMPT })
+
+const registerSuccess = () =>
+({ type: Types.REGISTER_SUCCESS })
+
+const registerFailure = () =>
+({ type: Types.REGISTER_FAILURE })
 
 const startup = () => ({ type: Types.STARTUP })
 
