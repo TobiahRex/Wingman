befdfb893c24c1ec4a8a2114d49bfa35ff354606
TobiahRex <tobiah.rex@gmail.com>
2016-09-05 21:56:16 -0700

    added an export to index.ios for "dispatch"

---
+++ App/Config/FirebaseConfig.js
@@ -0,0 +1,4 @@
+import Types from '../Actions/Types'
+import Actions from '../Actions/Creators'
+import { Actions as NavigationActions } from 'react-native-router-flux'
+import { dispatch } from '../../index.ios'
